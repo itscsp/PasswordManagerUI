fd7af510d91ba83c0e13c3b5e62e581a16d32b52
itscsp <chethanpoojary1@gmail.com>
2024-03-30 01:04:26 +0530

    favicon imaege changes

--- src/components/PasswordListItem.jsx
+++ src/components/PasswordListItem.jsx
@@ -6,14 +6,14 @@ export default function PasswordListItem({data}, key) {
     function DynamicFavicon({ website, ...props }) {
         const faviconUrl = `https://www.google.com/s2/favicons?sz=256&domain_url=${website}`;
 
-        return <img {...props} width="24" height="24" src={faviconUrl} />;
+        return <img {...props} width="16" height="16" src={faviconUrl} />;
     }
 
     return (
         
             <li key={key} className="  hover:bg-gray-100">
                 <Button isLink='true' className="flex  items-center" to={`/password/${data.id}`} >
-                    <div className="px-2 py-3">
+                    <div className="px-5 py-3">
                         <DynamicFavicon website={data.sites} />
 
                     </div>
